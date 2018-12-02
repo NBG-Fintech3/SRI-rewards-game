6eacde6130e4c2ef6dbd1e72b36c25901b581efe
Christos N. Stefanatos <c.stefanatos@acg.edu>
2018-12-02 11:44:06 +0200

    wrong commiit

--- sri_game.m
+++ sri_game.m
@@ -69,18 +69,19 @@ set(ha,'handlevisibility','off', ...
 handles.qcounter = 1;
 handles.score=0
 handles.results= cell(4,1);
-handles.results{1,1}=['Chris']
-handles.results{1,2}= ['Stefanatos']
-handles.results{1,3}= ['c.stefanatos@parityplatform.com']
-handles.results{1,4}= ['60'];
+userin = inputdlg({'Name','Surname','Email'},...
+              'Customer', [1 50; 1 50; 1 50]);
+          
+handles.results = userin ;
+handles.results{4,1}= ['0'];
 handles.qnum = 3;
 handles.UANSWER = cell(1,3);
-handles.ANSWER = cell(1,3)
-handles.atext= cell(3,3)
-handles.ANSWER{1,1} = char('option2')
-handles.ANSWER{1,2} = char('option2')
-handles.ANSWER{1,3} = char('option1')
-handles.qtext= cell(1,3)
+handles.ANSWER = cell(1,3) ;
+handles.atext= cell(3,3) ;
+handles.ANSWER{1,1} = char('option2');
+handles.ANSWER{1,2} = char('option2');
+handles.ANSWER{1,3} = char('option1');
+handles.qtext= cell(1,3);
 handles.qtext{1,1}= ['Socially responsible investing (SRI) is becoming more and more popular for both institutional and retail investors.  It involves :'];
 handles.qtext{1,2}= ['When comparing returns of two European Stock indices : MSCI Europe(general stock index) and MSCI Europe SRI ( exposure to companies with outstanding ESG ratings) :'];   
 handles.qtext{1,3}= ['Test Complete. The points you scored on the test have been credited to your "Go for More" account']
@@ -205,13 +206,19 @@ else
     handles.results{1,4}= num2str(final_score);
     fileID = fopen('Parity.txt','w');
      fprintf(fileID,'%-14s\r\n',[handles.results{1,1}]);
-     fprintf(fileID,'%-14s\r\n',[handles.results{1,2}]);
-     fprintf(fileID,'%-14s\r\n',[handles.results{1,3}]);
-     fprintf(fileID,'%-14s\r\n',[handles.results{1,4}]);
+     fprintf(fileID,'%-14s\r\n',[handles.results{2,1}]);
+     fprintf(fileID,'%-14s\r\n',[handles.results{3,1}]);
+     fprintf(fileID,'%-14s\r\n',[handles.results{4,1}]);
      fclose(fileID);
      set(handles.option1, 'String',[(handles.atext{3,1})])
  set(handles.option2, 'String',[(handles.atext{3,2})])
  set(handles.option3, 'String',[(handles.atext{3,3})])
+import matlab.net.*
+import matlab.net.http.*
+r = matlab.net.http.RequestMessage;
+uri = URI('http://localhost:57000/api/nbg/test');
+resp = send(r,uri);
+status = resp.StatusCode
      handles.gameover=1
 end
 
@@ -248,9 +255,9 @@ else
     handles.results{1,4}= num2str(final_score);
     fileID = fopen('Parity.txt','w');
      fprintf(fileID,'%-14s\r\n',[handles.results{1,1}]);
-     fprintf(fileID,'%-14s\r\n',[handles.results{1,2}]);
-     fprintf(fileID,'%-14s\r\n',[handles.results{1,3}]);
-     fprintf(fileID,'%-14s\r\n',[handles.results{1,4}]);
+     fprintf(fileID,'%-14s\r\n',[handles.results{2,1}]);
+     fprintf(fileID,'%-14s\r\n',[handles.results{3,1}]);
+     fprintf(fileID,'%-14s\r\n',[handles.results{4,1}]);
      fclose(fileID);
      set(handles.option1, 'String',[(handles.atext{handles.qcounter,1})])
  set(handles.option2, 'String',[(handles.atext{handles.qcounter,2})])
@@ -292,9 +299,9 @@ else
  set(handles.option3, 'String',[(handles.atext{handles.qnum,3})]);
      fileID = fopen('Parity.txt','w');
      fprintf(fileID,'%-14s\r\n',[handles.results{1,1}]);
-     fprintf(fileID,'%-14s\r\n',[handles.results{1,2}]);
-     fprintf(fileID,'%-14s\r\n',[handles.results{1,3}]);
-     fprintf(fileID,'%-14s\r\n',[handles.results{1,4}]);
+     fprintf(fileID,'%-14s\r\n',[handles.results{2,1}]);
+     fprintf(fileID,'%-14s\r\n',[handles.results{3,1}]);
+     fprintf(fileID,'%-14s\r\n',[handles.results{4,1}]);
      fclose(fileID);
 end
 % Update handles structure
